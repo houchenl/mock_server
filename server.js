68a7b02252ac9039b75e816aca3831a8d7d7d79d
刘磊 <liulei@yulin.local>
2017-03-27 14:35:30 +0800

    set tab == 4 spaces.

--- server.js
+++ server.js
@@ -8,48 +8,48 @@ const port = 9999;
 
 // create server
 const server = http.createServer((req, res) => {
-	var pathname = url.parse(req.url).pathname;
-	console.log(`request for ${pathname} received.`);
+    var pathname = url.parse(req.url).pathname;
+    console.log(`request for ${pathname} received.`);
 
-	var postData = '';
-	req.setEncoding('utf-8');
-	req.addListener('data', (chunk) => {
-		postData += chunk;
-		console.log(`receive post data ${chunk}.`);
-	});
-	req.addListener('end', () => {
-		mock(pathname, res, postData);
-	});
+    var postData = '';
+    req.setEncoding('utf-8');
+    req.addListener('data', (chunk) => {
+        postData += chunk;
+        console.log(`receive post data ${chunk}.`);
+    });
+    req.addListener('end', () => {
+        mock(pathname, res, postData);
+    });
 })
 
 // start server
 server.listen(port, host, () => {
-	console.log(`server running at ${host}:${port}.`);
+    console.log(`server running at ${host}:${port}.`);
 });
 
 // response read data
 function mock(param, res, postData) {
-	var filePath = paramToFileName(param);
+    var filePath = paramToFileName(param);
 
-	console.log(`about to read file ${filePath}`);
-	fs.readFile(filePath, (err, data) => {
-	  if (err) {
-		res.writeHead(404, {"Content-Type": "text/plain"});
-	    res.write("404 Not found");
-	    res.end();
-	  } else {
-	  	res.writeHead(200, {"Content-Type": "application/json"});
-	  	res.write(data);
-	  	res.end();
-	  }	  
-	});
+    console.log(`about to read file ${filePath}`);
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.write("404 Not found");
+            res.end();
+        } else {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(data);
+            res.end();
+        }
+    });
 }
 
 // get file name according to url's param.
 function paramToFileName(param) {
-	param = param.replace(/\//g, '_');
+    param = param.replace(/\//g, '_');
 
-	var pre = './files/';
-	var suf = '.json';
-	return pre + param + suf;
+    var pre = './files/';
+    var suf = '.json';
+    return pre + param + suf;
 }
